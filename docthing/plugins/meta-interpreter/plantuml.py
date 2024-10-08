f5a5f2fd60128472e8cf17de5d56731fbb9716cc
ferdiu <ferdiu.manzella@gmail.com>
2024-10-08 12:20:03 +0200

    add skeleton to definition of plugins
    Signed-off-by: ferdiu <ferdiu.manzella@gmail.com>

---
+++ docthing/plugins/meta-interpreter/plantuml.py
@@ -0,0 +1,7 @@
+
+from ..meta_interpreter import MetaInterpreter
+
+class PlantUMLInterpreter(MetaInterpreter):
+    def __init__(self, config):
+        super().__init__(config)
+        self.config = config
---
+++ docthing/plugins/meta_interpreter.py
@@ -0,0 +1,7 @@
+
+from abc import ABC, abstractmethod
+
+class MetaInterpreter(ABC):
+    """
+    MetaInterpreter is an abstract class that defines the interface for meta-interpreters.
+    """
